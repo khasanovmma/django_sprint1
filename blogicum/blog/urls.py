540fce98c4f30f78c0b27762f911bd5bbac8b5cd
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-01-13 20:58:22 +0500

    refact: fix E501 line too long

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -8,5 +8,9 @@ app_name = "blog"
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts/<int:pk>/", views.post_detail, name="post_detail"),
-    path("category/<slug:category_slug>/", views.category, name="category_posts"),
+    path(
+        "category/<slug:category_slug>/",
+        views.category,
+        name="category_posts",
+    ),
 ]
