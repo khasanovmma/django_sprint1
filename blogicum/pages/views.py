8314fab8ec864e3f9185daffcf2bb5934c7f8b40
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-01-13 20:25:43 +0500

    feat: implement pages about and rules

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def about(request):
+    template = "pages/about.html"
+    return render(request=request, template_name=template)
+
+def rules(request):
+    template = "pages/rules.html"
+    return render(request=request, template_name=template)
